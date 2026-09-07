902ad5607222b80d67a9d3f9ca6aed524615f9c8
chaudharykartik9 <tusharkartik8@gmail.com>
2026-09-07 20:25:26 +0530

    add sep folder

---
+++ sep-7/app/blog/[blogid]/page.tsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const Page = async ({ params }: any) => {
+  const { blogid } = await params;
+  const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${blogid}`);
+  const data = res.data ;
+
+
+  return <div>
+    <div>
+      yarr</div> 
+    data {data.title}
+    data {data.body}
+  </div>;
+};
+
+export default Page;
---
+++ sep-7/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      krish chutiya mere pass hai 
+    </div>
+    
+  );
+}
